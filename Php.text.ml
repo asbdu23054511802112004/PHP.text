70fbfa255b604cb81823699b34be70d5b5d8164d
R.Akshara <akshararagupathi@gmail.com>
2025-09-18 06:47:16 +0530

    Php.text.ml

---
+++ Php.text.ml
@@ -0,0 +1,29 @@
+<html>
+
+2-<head>
+
+3 <title>PHP</title>
+
+4 </head>
+
+5- <body>
+
+6 <center>
+
+7 <h1>PHP History </h1>
+
+8 </center>
+
+9-<p>In<mark>1995</mark>
+
+10 <i>Rasumus lerdorf</i>creates the first version of<b>PHP </b></p>
+
+11 <p>in <mark>1998</mark>a significant rewrite by <i >zeer suraski</i></p>
+
+12 <h2>content management system </h2>
+
+13 <p>platform like WordPress and drupal<em>various web servers </em></p>
+
+14 </body>
+
+15 </html>
